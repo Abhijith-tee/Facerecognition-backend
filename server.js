039645a580111cdbc6f9c1bd91ea6cf0132fce72
Abhijith T <abhijiththayad111@gmail.com>
2020-08-07 15:50:49 +0530

    Registering a user using postgres

--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const postgres = knex({
+const db = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
       user : 'postgres',
-      password : '',
+      password : 'test',
       database : 'smartbraindb'
     }
   });
 
-  postgres.select('*').from('users');
 
 const app = express();
 
@@ -57,13 +56,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
+    db('users').insert({
         email: email,
-        entries: 0,
+        name: name,
         joined: new Date()
-    })
+    }).then(console.log)
     res.json(database.users[database.users.length-1]);
 });
 
