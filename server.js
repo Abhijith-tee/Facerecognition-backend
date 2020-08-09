73d627a2593c6b3568fa1daef6d4384edcd3c3ff
Abhijith T <abhijiththayad111@gmail.com>
2020-08-09 12:44:47 +0530

    moving register endpoint to it's own file

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const { response } = require('express');
+// const { response, request } = require('express');
+
+const register = require('./controllers/register');
 
 const db = knex({
     client: 'pg',
@@ -44,33 +46,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('unable to get user'))
 });
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0],
-                name: name,
-                joined: new Date()
-            })
-            .then(user => {
-                res.json(user[0]);
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-     .catch(err => res.status(400).json('unable to register'))
-});
+app.post('/register',(req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req,res) => {
     const { id } = req.params;
@@ -99,3 +75,4 @@ app.put('/image', (req, res) => {
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
 });
+
