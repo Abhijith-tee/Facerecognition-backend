d126764b92601a9b46adb02a604d4698eeff3be0
Abhijith T <abhijiththayad111@gmail.com>
2020-08-09 13:23:07 +0530

    moving /image endpoint to controllers and cleaning up the code in server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-// const { response, request } = require('express');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
@@ -19,32 +19,16 @@ const db = knex({
     }
   });
 
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send(database.users);
-});
-
+app.get('/', (req, res) => {  res.send(database.users) });
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
-
 app.post('/register',(req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
